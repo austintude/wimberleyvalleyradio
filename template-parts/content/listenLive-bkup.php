667ea9e852c5940e2e2dcd56d2b14973f182d2f0
Daniel Bisett <daniel@austintatiousdesign.co>
2020-02-07 13:42:47 -0600

    replaced livestreaming iframe to stop double dipping w google analytics

--- template-parts/content/listenLive-bkup.php
+++ template-parts/content/listenLive-bkup.php
@@ -15,7 +15,7 @@ namespace WP_Rig\WP_Rig;
 <h2>
 		Listen Here Live
 	</h2>
-	<iframe class="desktop" frameborder="0" width="1240" height="830" scrolling="no" style="text-align:center; vertical-align:middle;" src="https://streamdb7web.securenetsystems.net/cirrusencore/KWVH"></iframe>
+	<iframe class="desktop" frameborder="0" width="1240" height="830" scrolling="no" style="text-align:center; vertical-align:middle;" src="https://radio.securenetsystems.net/radio_player_large.cfm?stationCallSign=KWVH"></iframe>
 		<?php $listenliveloop = new \WP_Query( array( 'post_type' => 'listen_live', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
 		<div class="mobile">
 <?php while( $listenliveloop->have_posts() ) : $listenliveloop->the_post();
