8da8efb5a9b5a7b4de5d2ebf1237af23de8fbec4
Development Director <development@Wimberley-Valley-Radios-iMac.local>
2020-02-21 12:15:06 -0600

    added miscelaneous blocks pulled in to the home page theough the acf pro flexible content

--- front-page.php
+++ front-page.php
@@ -19,16 +19,63 @@ if ( is_home() ) {
 }
 
 ?>
+
+
 	<main id="primary" class="site-main">
+	<?php $miscblockloop = new \WP_Query( array( 'post_type' => 'miscblockloop', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
+
+<?php while( $miscblockloop->have_posts() ) : $miscblockloop->the_post();
+			$blocka			= get_field('blocka');
+			?>
+		<?php if ($blocka != null) {
+			get_template_part( 'template-parts/content/blocka' );
+		} ?>
+			<?php endwhile;  wp_reset_query(); ?>
 		<?php
 			get_template_part( 'template-parts/content/block1' );
-			get_template_part( 'template-parts/content/block2' );
-			get_template_part( 'template-parts/content/block3' );
-			get_template_part( 'template-parts/content/block4' );
-			// get_template_part( 'template-parts/content/block5' );
 		?>
+		<?php while( $miscblockloop->have_posts() ) : $miscblockloop->the_post();
+			$blockb			= get_field('blockb');
+			?>
+		<?php if ($blockb != null) {
+			get_template_part( 'template-parts/content/blockb' );
+		} ?>
+			<?php endwhile;  wp_reset_query(); ?>
+		<?php
+			get_template_part( 'template-parts/content/block2' );
+		?>
+		<?php while( $miscblockloop->have_posts() ) : $miscblockloop->the_post();
+			$blockc			= get_field('blockc');
+			?>
+		<?php if ($blockc != null) {
+			get_template_part( 'template-parts/content/blockc' );
+		} ?>
+			<?php endwhile;  wp_reset_query(); ?>
+		<?php
+			get_template_part( 'template-parts/content/block3' );
+		?>
+		<?php while( $miscblockloop->have_posts() ) : $miscblockloop->the_post();
+			$blockd			= get_field('blockd');
+			?>
+		<?php if ($blockd != null) {
+			get_template_part( 'template-parts/content/blockd' );
+		} ?>
+			<?php endwhile;  wp_reset_query(); ?>
+		<?php
+			get_template_part( 'template-parts/content/block4' );
+		?>
+		<?php while( $miscblockloop->have_posts() ) : $miscblockloop->the_post();
+			$blocke			= get_field('blocke');
+			?>
+		<?php if ($blocke != null) {
+			get_template_part( 'template-parts/content/blocke' );
+		} ?>
+			<?php endwhile;  wp_reset_query(); ?>
 
 
 	</main><!-- #primary -->
+
+
 <?php
 get_footer();
+
