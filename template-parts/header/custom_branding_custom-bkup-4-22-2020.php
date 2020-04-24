319770553d251ae5798ad73dc32d0e8ad9b47f35
Daniel Bisett <development@wimberleyvalleyradio.org>
2020-04-23 22:00:25 -0500

    fix custom-page hero buttons and spacing

--- template-parts/header/custom_branding_custom-bkup-4-22-2020.php
+++ template-parts/header/custom_branding_custom-bkup-4-22-2020.php
@@ -51,28 +51,17 @@ $listen_live_tx			= get_field('listen_live_tx');
 $listen_live_link			= get_field('listen_live_link');
 $donate_txt			= get_field('donate_txt');
 $donate_link			= get_field('donate_link');
-$listen_live_x			= get_field('listen_live_x');
-$listen_live_x_link			= get_field('listen_live_x_link');
-$listen_live_x_onoff			= get_field('listen_live_x_onoff');
 
 			?>
 		<div class="listenLive">
 			<button>
-				<a href="<?php echo $listen_live_link; ?>" target="_blank" rel=”noreferrer”>
+				<a href="<?php echo $listen_live_link; ?>">
 			<?php echo $listen_live_tx; ?>
 </a>
-			</button>
-			</div><!-- listenLive -->
-			<?php if ($listen_live_x_onoff == 1) { ?>
-			<div class="listenLive kwvhX">
-			<button>
-				<a href="<?php echo $listen_live_x_link; ?>" target="_blank" rel=”noreferrer”>
-			<?php echo $listen_live_x; ?>
-</a>
+			<!-- <img src="<?php echo $listen_live_icon['url']; ?>" alt="<?php echo $listen_live_icon['alt']; ?>"/> -->
 			</button>
 
 </div><!-- listenLive -->
-			<?php } ?>
 <?php endwhile;  wp_reset_query(); ?>
 
 <div class="donateNow">
@@ -86,6 +75,8 @@ $listen_live_x_onoff			= get_field('listen_live_x_onoff');
 </div>
 </div>
 <?php endwhile;  wp_reset_query(); ?>
+
+</div>
 
 
 </div><!-- .site-branding -->
